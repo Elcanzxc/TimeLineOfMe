6bd1bcd8ddb5fbce24c6c296cb349ff52e481ab2
Elcan4361@gmail.com <Elcan4361@gmail.com>
2026-01-08 20:39:39 +0400

    Created TLOM.Core && Model->Book with validation

---
+++ TimeLineOfMe/TimeLineOfMe.Core/Models/Book.cs
@@ -0,0 +1,74 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace TimeLineOfMe.Core.Models;
+
+public class Book
+{ 
+    public const int MAX_TITLE_LENGTH = 250;
+
+    public Guid Id { get; }
+
+    public string Title { get;} = string.Empty;
+
+    public string Author { get; } = string.Empty;
+
+    public string? Description { get; }
+
+    public DateTime PublishedDate { get; }
+
+
+
+    private Book(Guid id, string title, string author, string? description, DateTime publishedDate)
+    {
+        Id = id;
+        Title = title;
+        Author = author;
+        Description = description;
+        PublishedDate = publishedDate;
+    }
+
+
+    public static (Book book,string error) Create(Guid id, string title, string author, string? description, string publishedDateStr)
+    {
+
+        StringBuilder errorBuilder = new();
+
+        string format = "yyyy-MM-dd HH:mm:ss";
+
+        bool success = DateTime.TryParseExact(
+                publishedDateStr,
+                format,
+                CultureInfo.InvariantCulture, 
+                DateTimeStyles.None,
+                out DateTime publishedDate);
+
+
+        if (string.IsNullOrWhiteSpace(title))
+            errorBuilder.AppendLine("Title cannot be empty.");
+
+        if (title.Length > MAX_TITLE_LENGTH)
+            errorBuilder.AppendLine($"Title cannot exceed {MAX_TITLE_LENGTH} characters.");
+
+        if(string.IsNullOrWhiteSpace(author))
+            errorBuilder.AppendLine("Author cannot be empty.");
+
+        if (!success)
+            errorBuilder.AppendLine($"Published date must be in the format {format}.");
+
+        else if (publishedDate > DateTime.UtcNow)
+            errorBuilder.AppendLine("Published date cannot be in the future.");
+
+
+
+
+        var book = new Book(id, title, author, description, publishedDate);
+
+
+        return (book, errorBuilder.ToString());
+
+    }
+
+
+
+}
--- TimeLineOfMe/TimeLineOfMe.Server/obj/Debug/net10.0/TimeLineOfMe.Server.AssemblyInfo.cs
+++ TimeLineOfMe/TimeLineOfMe.Server/obj/Debug/net10.0/TimeLineOfMe.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeLineOfMe.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cec3dcc9635b4e6ff2be9f31e5f5c419ad46f62")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeLineOfMe.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeLineOfMe.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
