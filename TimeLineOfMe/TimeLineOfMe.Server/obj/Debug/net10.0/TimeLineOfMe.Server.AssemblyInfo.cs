2cd96f7a49d3e59de38a527e8644ce26d7727d8f
Elcan4361@gmail.com <Elcan4361@gmail.com>
2026-01-16 13:43:10 +0400

    Configurate react npm create in the beginning

--- TimeLineOfMe/TimeLineOfMe.Server/obj/Debug/net10.0/TimeLineOfMe.Server.AssemblyInfo.cs
+++ TimeLineOfMe/TimeLineOfMe.Server/obj/Debug/net10.0/TimeLineOfMe.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeLineOfMe.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6118984cbff13debd3e16fe4803169fab0e5e17b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4036a71c9dcdbaab0d502fa0fc6dc07a21527f6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeLineOfMe.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeLineOfMe.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
